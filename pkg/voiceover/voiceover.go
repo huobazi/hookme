513469ce0b5db82ce02008fb5cf884893852e6e0
Marble Wu <huobazi@gmail.com>
2020-09-11 20:33:14 +0800

    Change datetime format in the log

--- pkg/voiceover/voiceover.go
+++ pkg/voiceover/voiceover.go
@@ -42,9 +42,8 @@ func newVoiceOver() VoiceOver {
 	l.SetFormatter(&logrus.TextFormatter{
 		ForceColors:     true,
 		FullTimestamp:   true,
-		TimestampFormat: time.RFC822,
+		TimestampFormat: "2006-01-02 15:04:05",
 	})
-
 	l.AddHook(rotateFileHook)
 
 	return &logrusVoiceOver{log: l}
